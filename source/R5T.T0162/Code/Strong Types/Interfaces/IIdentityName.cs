8254cc85c90d7c7c2db986c9a287bc9dff84e1f0
davidcoats <david@getsafetycone.com>
2023-07-27 01:32:17 -0700

    Interim changes.

--- source/R5T.T0162/Code/Strong Types/Interfaces/IIdentityName.cs
+++ source/R5T.T0162/Code/Strong Types/Interfaces/IIdentityName.cs
@@ -7,7 +7,7 @@ using R5T.T0179;
 namespace R5T.T0162
 {
     /// <summary>
-    /// 
+    /// https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/xmldoc/#id-strings
     /// </summary>
     [StrongTypeMarker]
     public interface IIdentityName : ITyped<string>, IStrongTypeMarker
